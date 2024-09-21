66c3600254561dfc402e8c6939e05bf7285d104d
Ahnsukjoo <bos98@naver.com>
2024-09-21 14:30:51 +0900

    add: memberController(read user)

--- join/src/membership/MemberController.java
+++ join/src/membership/MemberController.java
@@ -1,5 +1,55 @@
 package membership;
 
-public class MemberController {
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/*")
+public class MemberController extends HttpServlet {
+    //컨트롤러의 역할: 브라우저에서 요청한 경로에 따른 서비스 제공
+
+    MemberDAO dao;
+
+    @Override
+    public void init() {
+        dao = new MemberDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        doHandle(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        doHandle(request, response);
+    }
+
+    protected void doHandle(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf");
+        String action = request.getPathInfo();
+        String nextPage = null;
+
+        if (action == null ||action.equals("listShow.do")) {
+            List<MemberVO> membersList = dao.readDB();
+            request.setAttribute("membersList", membersList);
+            nextPage="/views/listMembers.jsp";
+        }else {
+            nextPage = "/views/listMembers.jsp";
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+        dispatcher.forward(request, response);
+    }
+
 
 }
